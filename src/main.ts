5e77cf883cc2c967f5561cb30bc66d5fe339ebc7
abdulmalik alsufayran <alsufiran@gmail.com>
2024-02-08 17:39:53 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,8 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+/* Tailwind Sheet */
+import './assets/tailwind.css';
 
 const app = createApp(App)
   .use(IonicVue)
---
+++ src/views/SettingsPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <ion-page>
+        <ion-content>
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-buttons>
+                        <ion-back-button defaultHref="/"></ion-back-button>
+                    </ion-buttons>
+                    <ion-title>Settings</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <div class="my-7">
+                <ion-list>
+  <ion-item>
+    <ion-label>Pokémon Yellow</ion-label>
+  </ion-item>
+</ion-list>
+            </div>
+        </ion-content>
+    </ion-page>
+</template>
+<script setup lang="ts">
+import { IonPage, IonContent, IonList, IonItem, IonLabel, IonTitle, IonHeader, IonToolbar, IonBackButton, IonButtons } from '@ionic/vue';
+</script>
